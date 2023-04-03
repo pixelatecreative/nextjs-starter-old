ca9bb0a3f30ce8eda0b13255dfe8ca69ca995b3e
Onder Bakirtas <onderbakirtas@gmail.com>
2023-04-03 17:01:32 +0300

    feat(app): added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: '',
+  eslint: {
+    dirs: ['.'],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "@/styles/config/index.scss";`,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
