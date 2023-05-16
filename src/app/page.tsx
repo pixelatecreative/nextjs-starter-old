31eb988d955dce38b043962f80fc07e24f8e369f
Onder Bakirtas <onderbakirtas@gmail.com>
2023-05-16 16:44:35 +0300

    feat: added scss modules example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import type { Metadata } from 'next';
 
 import { AppConfig } from '@/utils/app-config';
 
+import styles from './page.module.scss';
+
 export const metadata: Metadata = {
   title: AppConfig.title,
   description: AppConfig.description,
 };
 
 export default function Page() {
-  return <h1>{AppConfig.title}</h1>;
+  return <h1 className={styles.h1}>{AppConfig.title}</h1>;
 }
